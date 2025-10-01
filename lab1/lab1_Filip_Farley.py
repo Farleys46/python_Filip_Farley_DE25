7203fde2e32add98b4540f4bd176a0fc5ac0f9b3
Filip Farley <filip@farley.se>
2025-10-01 11:10:41 +0200

    update

---
+++ lab1/lab1_Filip_Farley.py
@@ -0,0 +1,6 @@
+# Task 1
+
+#I want to store the data into variables.
+
+seq1 = cGTAaccaataaaaaaacaagcttaacctaattc
+seq2 = agcttagTTTGGatctggccgggg
---
+++ test.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "orange", "grape", "strawberry"]
+
+print("My favorite fruits are: ")
+for i, fruit in enumerate(fruits,0):
+        print(f"{i}. {fruit}")
