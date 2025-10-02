085209c5cb0937a2634c9b4d984bdfb294b3d9fa
Filip Farley <filip@farley.se>
2025-10-02 08:57:42 +0200

    exercise updates

--- lab1/lab1_Filip_Farley.py
+++ lab1/lab1_Filip_Farley.py
@@ -1,6 +1,14 @@
 # Task 1
 
-#I want to store the data into variables.
+#I want to store the data into lists.
 
-seq1 = cGTAaccaataaaaaaacaagcttaacctaattc
-seq2 = agcttagTTTGGatctggccgggg
+seq1 = "cGTAaccaataaaaaaacaagcttaacctaattc"
+seq2 = "agcttagTTTGGatctggccgggg"
+seq3 = "gcggatttactcCCCCCAAAAANNaggggagagcccagataaatggagtctgtgcgtccaca"
+seq4 = "gcggatttactcaggggagagcccagGGataaatggagtctgtgcgtccaca"
+
+a1 = seq1.lower().count("a")
+t1 = seq1.lower().count("t")
+c1 = seq1.lower().count("c")
+g1 = seq1.lower().count("g")
+print(f"Sequence 1 has the letter A {a1} times, T {t1} times, C {c1} times and G {g1} times.")
--- test.py
+++ test.py
@@ -1,5 +1,23 @@
-fruits = ["apple", "orange", "grape", "strawberry"]
+import matplotlib.pyplot as plt
 
-print("My favorite fruits are: ")
-for i, fruit in enumerate(fruits,0):
-        print(f"{i}. {fruit}")
+sequences = {
+    "Sequence 1": "cGTAaccaataaaaaaacaagcttaacctaattc",
+    "Sequence 2": "agcttagTTTGGatctggccgggg",
+    "Sequence 3": "gcggatttactcCCCCCAAAAANNaggggagagcccagataaatggagtctgtgcgtccaca",
+    "Sequence 4": "gcggatttactcaggggagagcccagGGataaatggagtctgtgcgtccaca"
+}
+
+for name, seq in sequences.items():
+    seq = seq.lower()
+    counts = {
+        "A": seq.count("a"),
+        "T": seq.count("t"),
+        "C": seq.count("c"),
+        "G": seq.count("g")
+    }
+
+    plt.bar(counts.keys(), counts.values())
+    plt.title(f"DNA Letter Frequency - {name}")
+    plt.xlabel("DNA Letter")
+    plt.ylabel("Count")
+    plt.show()
