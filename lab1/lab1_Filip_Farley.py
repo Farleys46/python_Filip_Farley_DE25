3c416842959711f211ea029e952b6c5ad95dbff1
Filip Farley <filip@farley.se>
2025-10-08 11:33:15 +0200

    update file handling

--- lab1/lab1_Filip_Farley.py
+++ lab1/lab1_Filip_Farley.py
@@ -1,14 +1,28 @@
 # Task 1
 
-#I want to store the data into lists.
+import matplotlib.pyplot as plt 
+# Open the file, and use ".readlines" to read each line and put them in a list
+with open("lab1/dna_raw.txt", "r") as file:
+    contents = file.readlines()
+    
 
-seq1 = "cGTAaccaataaaaaaacaagcttaacctaattc"
-seq2 = "agcttagTTTGGatctggccgggg"
-seq3 = "gcggatttactcCCCCCAAAAANNaggggagagcccagataaatggagtctgtgcgtccaca"
-seq4 = "gcggatttactcaggggagagcccagGGataaatggagtctgtgcgtccaca"
+# Creating a dictionary to store the results later. 
+dna_counts = {}
 
-a1 = seq1.lower().count("a")
-t1 = seq1.lower().count("t")
-c1 = seq1.lower().count("c")
-g1 = seq1.lower().count("g")
-print(f"Sequence 1 has the letter A {a1} times, T {t1} times, C {c1} times and G {g1} times.")
+#First created a for loop with a jump of 2 at a time then remove the "\n" from the file to clean it.
+#Then 
+for i in range(0, len(contents), 2):
+    seq_number = contents[i].strip()
+    dna_letters = contents[i + 1].strip().lower()
+    
+    counts = {
+        "a": dna_letters.count("a"),
+        "t": dna_letters.count("t"),
+        "c": dna_letters.count("c"),
+        "g": dna_letters.count("g")
+    }
+
+    dna_counts[seq_number] = counts
+
+for seq, counts in dna_counts.items():
+    print(f"{seq}: {counts}")  
