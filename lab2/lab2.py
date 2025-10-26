785efbaccc81609913dcbca92fd5503bc97d6dc7
Filip Farley <filip@farley.se>
2025-10-26 11:27:37 +0100

    lab2 start

---
+++ lab2/lab2.py
@@ -0,0 +1,11 @@
+from circle import circle
+from rectangle import rectangle
+
+class Shape:
+    def __init__(self, position_x: int|float, position_y: int|float, area: int|float, perimeter: int|float):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.area = area
+        self.perimeter = perimeter
+        
+    def 
